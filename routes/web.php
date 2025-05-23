51475e570393be86671ae75eb30bf784a2c4097f
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-23 09:20:51 +0530

    p

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/hospital', function () {
-    return view('hospital.manage');
-});
+// Route::get('/hospital', function () {
+//     return view('hospital.manage');
+// });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
