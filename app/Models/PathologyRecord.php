2c4a65e5066e20fc78cd6cf8b5972ec0686613f2
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2026-03-27 13:10:18 +0530

    Update PathologyRecord.php

--- app/Models/PathologyRecord.php
+++ app/Models/PathologyRecord.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class PathologyRecord extends Model
 {
-    //
+    protected $fillable = [
+        'patient_id',
+        'pathology_test_id',
+        'doctor_id',
+        'test_date',
+        'result',
+        'status'
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function pathologyTest()
+    {
+        return $this->belongsTo(PathologyTest::class, 'pathology_test_id');
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
