a75650c8ad95a4974a28735aa5c49f908d3bfca9
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2026-03-27 13:10:02 +0530

    Update MedicineSale.php

--- app/Models/MedicineSale.php
+++ app/Models/MedicineSale.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicineSale extends Model
 {
-    //
+    protected $fillable = [
+        'patient_id',
+        'medicine_id',
+        'quantity',
+        'total_amount',
+        'sale_date'
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function medicine()
+    {
+        return $this->belongsTo(Medicine::class);
+    }
 }
