a5d8d352517b91308814549b5bfdce2ee04924e0
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2026-03-27 12:55:06 +0530

    Update 2026_03_10_115337_create_medicines_table.php

--- database/migrations/2026_03_10_115337_create_medicines_table.php
+++ database/migrations/2026_03_10_115337_create_medicines_table.php
@@ -11,16 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('medicines', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('category');
-            $table->string('manufacturer');
-            $table->decimal('price', 10, 2);
-            $table->integer('stock_quantity');
-            $table->date('expiry_date');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('medicines')) {
+            Schema::create('medicines', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('category');
+                $table->string('manufacturer');
+                $table->decimal('price', 10, 2);
+                $table->integer('stock_quantity');
+                $table->date('expiry_date');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
